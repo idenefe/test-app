a385865aeeee306553cccd21ce39c1f9732c6c24
Rails Udemy Course ID <idenefe@gmail.com>
2016-09-13 23:51:41 +0000

    bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get 'welcome/home', to: 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
-  get 'welcome/index', to: 'welcome#index'
+  get 'welcome', to: 'welcome#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
